5f584f6b94c0f02d8ccd258112c0231893a7bf7c
POII09 <2671858186@qq.com>
2025-12-19 10:16:57 +0800

    修改地图生成算法

--- Classes/Scene/Prison.h
+++ Classes/Scene/Prison.h
@@ -2,18 +2,29 @@
 #include "cocos2d.h"
 #include "Map/SceneMap.h"
 #include "People/Player.h"
+#define CORR_WIDTH 10
+#define PUSH_FORCE 10.0f
+#define PULL_FORCE 5.0f
+#define MAX_TEMP 10000
+#define BLOCK_WEIGHT 1000000
+#define SURROUND_WEIGHT 10
 USING_NS_CC;
 
 class Prison
 {
 public:
 
+	void SetupVisualScene();
 	bool InitPrisonData();
 	bool RenderPrisonScene();
+	void GenRoomPos();
 
 	SceneMap* PrisonMap = nullptr;
 	SceneMapData* PrisonMapData = nullptr;
 	Scene* PrisonScene = nullptr;
-	//Player* _player = nullptr;
+	MapDataManager* mDM = nullptr;
+	cocos2d::DrawNode* _debugDrawNode = nullptr;
+	std::vector<std::vector<int>> MapArray;
+	void Draw();
 
 };
