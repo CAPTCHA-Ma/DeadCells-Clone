6d2855c70b8e1274b24a451c7125ddcdd3dcc709
KBQ118 <KBQ118@users.noreply.github.com>
2025-12-26 19:42:36 +0800

    Add TMX room preview, TileRenderer, and prison textures

---
+++ Resources/prison/tilesets/prison_chains.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="prison_chains" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="cageChain">
+  <image width="24" height="24" source="../chains/cageChain-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="cageChain"/>
+  </properties>
+ </tile>
+ <tile id="1" type="chainAnchor">
+  <image width="48" height="48" source="../chains/chainAnchor-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainAnchor"/>
+  </properties>
+ </tile>
+ <tile id="2" type="chainBot_0">
+  <image width="48" height="96" source="../chains/chainBot_0-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainBot_0"/>
+  </properties>
+ </tile>
+ <tile id="3" type="chainBot_1">
+  <image width="48" height="72" source="../chains/chainBot_1-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainBot_1"/>
+  </properties>
+ </tile>
+ <tile id="4" type="chainBot_2">
+  <image width="48" height="96" source="../chains/chainBot_2-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainBot_2"/>
+  </properties>
+ </tile>
+ <tile id="5" type="chainBot_3">
+  <image width="48" height="72" source="../chains/chainBot_3-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainBot_3"/>
+  </properties>
+ </tile>
+ <tile id="6" type="chainDown">
+  <image width="24" height="24" source="../chains/chainDown-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainDown"/>
+  </properties>
+ </tile>
+ <tile id="7" type="chainLight">
+  <image width="24" height="48" source="../chains/chainLight-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainLight"/>
+  </properties>
+ </tile>
+ <tile id="8" type="chainShadow">
+  <image width="24" height="24" source="../chains/chainShadow-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainShadow"/>
+  </properties>
+ </tile>
+ <tile id="9" type="chainTile">
+  <image width="24" height="24" source="../chains/chainTile-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainTile"/>
+  </properties>
+ </tile>
+ <tile id="10" type="chainUp">
+  <image width="24" height="48" source="../chains/chainUp-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chainUp"/>
+  </properties>
+ </tile>
+ <tile id="11" type="chain_0">
+  <image width="24" height="24" source="../chains/chain_0-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="chain_0"/>
+  </properties>
+ </tile>
+ <tile id="12" type="maillon">
+  <image width="6" height="8" source="../chains/maillon-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="maillon"/>
+  </properties>
+ </tile>
+ <tile id="13" type="suspendedCage">
+  <image width="27" height="48" source="../chains/suspendedCage-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="chains"/>
+   <property name="name" value="suspendedCage"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ Resources/prison/tilesets/prison_webs.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.0" name="prison_webs" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="webBigBL">
+  <image width="48" height="48" source="../webs/webBigBL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webBigBL"/>
+  </properties>
+ </tile>
+ <tile id="1" type="webBigBR">
+  <image width="48" height="48" source="../webs/webBigBR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webBigBR"/>
+  </properties>
+ </tile>
+ <tile id="2" type="webBigTL">
+  <image width="48" height="48" source="../webs/webBigTL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webBigTL"/>
+  </properties>
+ </tile>
+ <tile id="3" type="webBigTR">
+  <image width="48" height="48" source="../webs/webBigTR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webBigTR"/>
+  </properties>
+ </tile>
+ <tile id="4" type="webMediumBL">
+  <image width="48" height="24" source="../webs/webMediumBL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webMediumBL"/>
+  </properties>
+ </tile>
+ <tile id="5" type="webMediumBR">
+  <image width="48" height="24" source="../webs/webMediumBR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webMediumBR"/>
+  </properties>
+ </tile>
+ <tile id="6" type="webMediumTL">
+  <image width="48" height="24" source="../webs/webMediumTL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webMediumTL"/>
+  </properties>
+ </tile>
+ <tile id="7" type="webMediumTR">
+  <image width="48" height="24" source="../webs/webMediumTR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webMediumTR"/>
+  </properties>
+ </tile>
+ <tile id="8" type="webSmallBL">
+  <image width="24" height="24" source="../webs/webSmallBL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webSmallBL"/>
+  </properties>
+ </tile>
+ <tile id="9" type="webSmallBR">
+  <image width="24" height="24" source="../webs/webSmallBR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webSmallBR"/>
+  </properties>
+ </tile>
+ <tile id="10" type="webSmallTL">
+  <image width="24" height="24" source="../webs/webSmallTL-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webSmallTL"/>
+  </properties>
+ </tile>
+ <tile id="11" type="webSmallTR">
+  <image width="24" height="24" source="../webs/webSmallTR-=-0-=-.png"/>
+  <properties>
+   <property name="category" value="webs"/>
+   <property name="name" value="webSmallTR"/>
+  </properties>
+ </tile>
+</tileset>
